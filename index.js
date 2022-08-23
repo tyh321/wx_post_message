34b786fefbc609a69f6bdc9f13e59cc3cf81674f
田玉杭 <tianyuhang@shun178.com>
2022-08-23 11:36:32 +0800

    优化

--- index.js
+++ index.js
@@ -38,8 +38,10 @@
 
 	try {
 		const { date, weather, min_temp, max_temp, love_day, birthday, words } = TEMPLATE_DATA.data;
+
 		// date
 		date.value = dayjs().format('YYYY-MM-DD') + ' 星期' + '日一二三四五六'.charAt(new Date().getDay());
+
 		// 天气
 		const { data: weathersRes } = await axios.get(`http://autodev.openspeech.cn/csp/api/v2.1/weather`, {
 			params: {
@@ -53,22 +55,28 @@
 		weather.value = _weather;
 		min_temp.value = low + '°C';
 		max_temp.value = high + '°C';
+
 		// 恋爱天数
 		love_day.value = dayjs(new Date()).diff(dayjs(START_DATE), 'days');
+
 		// 生日
 		const birTime = utils.getBirTime(BIRTHDAY);
 		birthday.value = Math.abs(dayjs(birTime).diff(dayjs(new Date()), 'days'));
+
 		// 彩虹屁
 		const { data: wordRes } = await axios.get('https://api.shadiao.pro/chp');
 		words.value = wordRes.data.text;
+
 		// 随机颜色
 		for (const key in TEMPLATE_DATA.data) {
 			TEMPLATE_DATA.data[key].color = utils.randomColor();
 		}
+
 		// 获取access_token;
 		const { data: accessTokenRes } = await axios.get(
 			`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${APP_ID}&secret=${APP_SECRET}`
 		);
+		
 		// 消息推送
 		await axios.post(`https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=${accessTokenRes.access_token}`, TEMPLATE_DATA);
 	} catch (error) {
